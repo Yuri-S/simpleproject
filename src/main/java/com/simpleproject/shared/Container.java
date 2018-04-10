9e46819fabc71773361a7103830ead5ca7dcc50e
ANazarov <ANazarov@MININT-9O1BMI8.ntv-tv.ru>
2018-04-10 19:15:58 +0300

    Container

---
+++ src/main/java/com/simpleproject/shared/Container.java
@@ -0,0 +1,21 @@
+package com.simpleproject.shared;
+
+public class Container {
+
+	Container() {};
+	
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+
+	private String string;
+	
+	
+	
+	
+	
+}

319ce61f3801ebd9c1e7acbc26c8d11f787dba63
YSergeev <YSergeev@>
2018-04-11 13:53:15 +0300

    visible

--- src/main/java/com/simpleproject/client/Simpleproject.java
+++ src/main/java/com/simpleproject/client/Simpleproject.java
@@ -80,9 +80,12 @@ public class Simpleproject implements EntryPoint {
     
     dialogVPanel.add(new Label("New branch"));
     dialogVPanel.add(new Label("added New branch"));
+    
+    
     Label n  = new Label("Another Label");
     
     dialogVPanel.add(n);
+    n.setVisible(false);
     dialogBox.setWidget(dialogVPanel);
 
     // Add a handler to close the DialogBox
